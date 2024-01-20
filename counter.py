9ededb50c8f1ac815f1bd432a26a57a6dd09afa6
chsze02 <157186521+chsze02@users.noreply.github.com>
2024-01-20 11:47:31 -0800

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,36 +1,59 @@
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 
-class childApp(GridLayout):
-    def __init__(self, **kwargs):
-        super(childApp, self).__init__(**kwargs)
-        self.cols = 2
+Builder.load_file('whatever.kv')
+class MyGrid(Widget):
+
+
+    name = ObjectProperty(None)
+    pizza = ObjectProperty(None)
+    '''def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+
+        self.cols = 1
+
+        self.top_grid = GridLayout()
         
-        self.add_widget(Label(text = "Student Name"))
-        self.s_name = TextInput()
-        self.add_widget(self.s_name)
+        self.top_grid.cols = 2
 
         self.add_widget(Image(source = "Slug.png"))
+        
+        self.top_grid.add_widget(Label(text = "Student Name"))
+        self.s_name = TextInput(multiline = False)
+        self.top_grid.add_widget(self.s_name)
 
 
-        self.add_widget(Label(text = "Student Gender"))
+        self.top_grid.add_widget(Label(text = "Student Gender"))
         self.s_gender = TextInput()
-        self.add_widget(self.s_gender)
+        self.top_grid.add_widget(self.s_gender)
 
-        self.press = Button(text = 'click me')
+        self.add_widget(self.top_grid)
+
+        self.press = Button(text = "CLICK ME", font_size = 40, size_hint_y = None, height = 100)
         self.press.bind(on_press = self.click_me)
-        self.add_widget(self.press)
+        self.add_widget(self.press)'''
 
-    def click_me(self, instance):
-        print(f"name of student is {self.s_name.text}")
+    def press(self):
+        name = self.name.text
+        pizza = self.pizza.text
+        print(f'{name} and {pizza}')
 
-class parentApp(App):
+        self.name.text = ""
+        self.pizza.text = ""
+        
+        '''if self.name.text != '':
+            print(f"name of student is {self.name.text}")
+            self.add_widget(Label(text = "thank you for your input"))
+
+            self.s_name.text = '''''
+
+
+class Awe(App):
     def build(self):
-        return childApp()
+        return MyGrid()
 if __name__ == "__main__":
-    parentApp().run()
+    Awe().run()
+
