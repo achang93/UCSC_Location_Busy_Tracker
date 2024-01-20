337a9cca5ba62eb3488204b85ae5785641679edb
limeconcretepowder <116107071+limeconcretepowder@users.noreply.github.com>
2024-01-20 00:18:59 -0800

    n

---
+++ UCSC_populartimes.py
@@ -0,0 +1,5 @@
+import googlemaps
+import populartimes.populartimes as pt
+
+if __name__ == "__main__":
+    x = pt.get_populartimes('AIzaSyAdmbXNRA37N8dkheO_6A1t2DVhTt8I4tI', 'ChIJk7eypKFBjoAR87wpNgAjQek')
